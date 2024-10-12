14a6eb13a36635174ff08af5af29cff273b3cbe0
JJB9922 <JJB9922@protonmail.com>
2024-10-12 22:57:21 +0100

    Add gui stuff

--- build.zig
+++ build.zig
@@ -1,18 +1,11 @@
 const std = @import("std");
-pub fn build(b: *std.Build) void {
+// Import the project from zon file.
+const rlzb = @import("raylib-zig-bindings");
+
+pub fn build(b: *std.Build) !void {
+    // Default zig setup
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
-
-    const lib = b.addStaticLibrary(.{
-        .name = "Circles",
-
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "Circles",
@@ -21,38 +14,51 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.linkSystemLibrary("raylib");
-    exe.linkLibC();
+    // Adding rlzb binding files for us to use in the main.zig file.
+    const bindings = b.dependency("raylib-zig-bindings", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("rlzb", bindings.module("raylib-zig-bindings"));
+
+    // Compiling raylib with main.zig
+    // You can select which raylib C file to add in the third parameter
+    var setup = try rlzb.Setup.init(b, .{ .cwd_relative = "raylib/src" }, .{});
+    defer setup.deinit();
+
+    // This line copy the raygui.h file into raylib/src to build with it.
+    try setup.addRayguiToRaylibSrc(b, .{ .cwd_relative = "raygui/src/raygui.h" });
+
+    // If you have some raylib's C #define requirements that need to be at build time.
+    // You can set them here:
+    // setup.setRayguiOptions(b, exe, .{});
+    // setup.setRCameraOptions(b, exe, .{});
+    // setup.setRlglOptions(b, exe, .{});
+
+    // Note:
+    // Some target needs specific opengl api version (RlglOption).
+    // For example linux .platform = DRM requires opengl_es2
+    // If you do not uncomment the setup.setRlglOptions above.
+    // It will add it automatically when linking.
+
+    // Build specific for platform.
+    switch (target.result.os.tag) {
+        .windows => try setup.linkWindows(b, exe),
+        .macos => try setup.linkMacos(b, exe),
+        .linux => try setup.linkLinux(b, exe, .{ .platform = .DESKTOP, .backend = .X11 }),
+        else => @panic("Unsupported os"),
+    }
+
+    // Add everything to the exe.
+    setup.finalize(b, exe);
+
+    // Default zig build run command setup
     b.installArtifact(exe);
-
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,77 +1,117 @@
 const std = @import("std");
-const rl = @cImport({
-    @cInclude("raylib.h");
-});
+const rlzb = @import("rlzb");
+const rl = rlzb.raylib;
+const rg = rlzb.raygui;
+
+const ShapeType = enum {
+    circle,
+};
+
+const Shape = struct {
+    type: ShapeType,
+    origin: rl.Vector2,
+    outerBound: f32,
+};
+
+const SelectedTool = enum {
+    none,
+    draw_circle,
+};
+
+var selectedTool: SelectedTool = SelectedTool.none;
+
+fn draw_ui(shapeList: *std.ArrayList(Shape)) !void {
+    rl.DrawFPS(2, 64);
+    rl.DrawText("Circles Present:", 2, 86, 22, rl.Fade(rl.DARKBLUE, 0.8));
+
+    const n = shapeList.items.len;
+    var buf: [10]u8 = undefined;
+    const str = try std.fmt.bufPrintZ(&buf, "{}", .{n});
+
+    rl.DrawText(str, 2, 112, 22, rl.Fade(rl.DARKBLUE, 0.8));
+    rl.DrawLine(0, 64, rl.GetScreenWidth(), 64, rl.Fade(rl.BLACK, 0.3));
+    rl.DrawRectangle(0, 0, rl.GetScreenWidth(), 64, rl.Fade(rl.LIGHTGRAY, 0.8));
+
+    const circleBtn = rl.Rectangle{ .x = 8, .y = 8, .width = 86, .height = 48 };
+    if (rg.GuiButton(circleBtn, "Circle Tool") == 1) {
+        selectedTool = SelectedTool.draw_circle;
+    }
+}
+
+fn draw_circle_mode(isDrawingShape: *bool, drawOrigin: *rl.Vector2, drawOuterBound: *f32, mousePos: *const rl.Vector2, shapeList: *std.ArrayList(Shape)) !void {
+    if (rl.IsMouseButtonPressed(rl.MOUSE_LEFT_BUTTON.toCInt()) and !isDrawingShape.* and mousePos.y > 64) {
+        drawOrigin.* = rl.Vector2{ .x = mousePos.*.x, .y = mousePos.*.y };
+        isDrawingShape.* = true;
+    }
+
+    if (isDrawingShape.*) {
+        drawOuterBound.* = std.math.sqrt(std.math.pow(f32, (mousePos.*.x - drawOrigin.*.x), 2.0) + std.math.pow(f32, (mousePos.*.y - drawOrigin.*.y), 2.0));
+
+        rl.DrawCircleSector(drawOrigin.*, drawOuterBound.*, 0, 360, 32, rl.Fade(rl.RED, 0.3));
+
+        if (drawOuterBound.* > 1.0 and rl.IsMouseButtonPressed(rl.MOUSE_LEFT_BUTTON.toCInt())) {
+            isDrawingShape.* = false;
+            const newCircle = Shape{ .type = ShapeType.circle, .origin = drawOrigin.*, .outerBound = drawOuterBound.* };
+
+            try shapeList.*.append(newCircle);
+        }
+    }
+
+    for (shapeList.*.items) |c| {
+        rl.DrawCircleSector(c.origin, c.outerBound, 0, 360, 32, rl.Fade(rl.BLUE, 0.3));
+    }
+
+    if (isDrawingShape.*) {
+        const deltaX = (mousePos.*.x - drawOrigin.*.x);
+        const deltaY = (mousePos.*.y - drawOrigin.*.y);
+
+        var stPointX: i32 = @intFromFloat(drawOrigin.*.x);
+        var stPointY: i32 = @intFromFloat(drawOrigin.*.y);
+
+        const iter: usize = @intFromFloat(drawOuterBound.* / 2);
+
+        for (0..iter) |_| {
+            const distanceToCenter = std.math.sqrt(std.math.pow(f32, @as(f32, @floatFromInt(stPointX)) - drawOrigin.*.x, 2) +
+                std.math.pow(f32, @as(f32, @floatFromInt(stPointY)) - drawOrigin.*.y, 2));
+
+            if (distanceToCenter <= drawOuterBound.*) {
+                rl.DrawPixel(stPointX, stPointY, rl.WHITE);
+            }
+
+            stPointX += @as(i32, @intFromFloat(deltaX / 16));
+            stPointY += @as(i32, @intFromFloat(deltaY / 16));
+        }
+    }
+}
 
 pub fn main() !void {
-    const winWidth: i32 = 800;
-    const winHeight: i32 = 450;
+    const winWidth: i32 = 1200;
+    const winHeight: i32 = 850;
     const winTitle = "Circles";
     rl.InitWindow(winWidth, winHeight, winTitle);
 
-    const circle = struct { center: rl.Vector2, outerRadius: f32, startAngle: f32, endAngle: f32, segments: f32 };
-
     rl.SetTargetFPS(120);
 
-    var isDrawingCircle: bool = false;
-    var drawCenter = rl.Vector2{ .x = 0.0, .y = 0.0 };
-    var drawOuterRadius: f32 = 0.0;
+    var isDrawingShape: bool = false;
+    var drawOrigin = rl.Vector2{ .x = 0.0, .y = 0.0 };
+    var drawOuterBound: f32 = 0.0;
 
     const listAllocator = std.heap.page_allocator;
-    var circleList = std.ArrayList(circle).init(listAllocator);
-    defer circleList.deinit();
+    var shapeList = std.ArrayList(Shape).init(listAllocator);
+    defer shapeList.deinit();
 
     while (!rl.WindowShouldClose()) {
         const mousePos: rl.Vector2 = rl.GetMousePosition();
 
         rl.BeginDrawing();
-
         rl.ClearBackground(rl.RAYWHITE);
 
-        if (rl.IsMouseButtonPressed(rl.MOUSE_BUTTON_LEFT) and !isDrawingCircle) {
-            drawCenter = rl.Vector2{ .x = mousePos.x, .y = mousePos.y };
-            isDrawingCircle = true;
+        switch (selectedTool) {
+            SelectedTool.none => {},
+            SelectedTool.draw_circle => try draw_circle_mode(&isDrawingShape, &drawOrigin, &drawOuterBound, &mousePos, &shapeList),
         }
 
-        if (isDrawingCircle) {
-            drawOuterRadius = std.math.sqrt(std.math.pow(f32, (mousePos.x - drawCenter.x), 2.0) + std.math.pow(f32, (mousePos.y - drawCenter.y), 2.0));
-
-            rl.DrawCircleSector(drawCenter, drawOuterRadius, 0, 360, 32, rl.Fade(rl.RED, 0.3));
-
-            if (drawOuterRadius > 1.0 and rl.IsMouseButtonPressed(rl.MOUSE_BUTTON_LEFT)) {
-                isDrawingCircle = false;
-                const newCircle = circle{ .center = drawCenter, .outerRadius = drawOuterRadius, .startAngle = 0.0, .endAngle = 360.0, .segments = 32.0 };
-
-                try circleList.append(newCircle);
-            }
-        }
-
-        for (circleList.items) |c| {
-            rl.DrawCircleSector(c.center, c.outerRadius, 0, 360, 32, rl.Fade(rl.BLUE, 0.3));
-        }
-
-        if (isDrawingCircle) {
-            const deltaX = (mousePos.x - drawCenter.x);
-            const deltaY = (mousePos.y - drawCenter.y);
-
-            var stPointX = @as(i32, @intFromFloat(drawCenter.x));
-            var stPointY = @as(i32, @intFromFloat(drawCenter.y));
-
-            const iter = @as(usize, @intFromFloat(drawOuterRadius / 2));
-
-            for (0..iter) |_| {
-                const distanceToCenter = std.math.sqrt(std.math.pow(f32, @as(f32, @floatFromInt(stPointX)) - drawCenter.x, 2) +
-                    std.math.pow(f32, @as(f32, @floatFromInt(stPointY)) - drawCenter.y, 2));
-
-                if (distanceToCenter <= drawOuterRadius) {
-                    rl.DrawPixel(stPointX, stPointY, rl.WHITE);
-                }
-
-                stPointX += @as(i32, @intFromFloat(deltaX / 16));
-                stPointY += @as(i32, @intFromFloat(deltaY / 16));
-            }
-        }
-
+        try draw_ui(&shapeList);
         rl.EndDrawing();
     }
 
